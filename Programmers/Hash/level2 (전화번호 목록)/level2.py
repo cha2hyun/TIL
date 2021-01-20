ee88cf629a3b0c2f0321458d433ce5115285f5a1
cha2hyun-dev <cha2hyun.dev@gmail.com>
2021-01-20 12:28:20 +0900

    20210120

--- Programmers/Hash/level2 (전화번호 목록)/level2.py
+++ Programmers/Hash/level2 (전화번호 목록)/level2.py
@@ -1,8 +1,8 @@
 def solution(phone_book): 
     answer = True 
     phone_book.sort()
-    for i in range(len(phone_book)-1)
-        if phone_book[i] in phone_book[i+1]
+    for i in range(len(phone_book)-1):
+        if phone_book[i] in phone_book[i+1]:
             answer = False
             return answer
     return answer
@@ -12,3 +12,11 @@ def solution(phone_book):
 phone_book = ["123","456","789"]
 a = solution(phone_book)
 print(a)
+
+# p = "123"
+# q = "124312534123"
+
+# if p in q:
+#     print("yes")
+# else:
+#     print("no")

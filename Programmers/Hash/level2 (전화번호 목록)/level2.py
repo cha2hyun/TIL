b044145709adb7f605ea41ac9f2541e23e5ca785
cha2hyun-dev <cha2hyun.dev@gmail.com>
2021-01-20 18:45:48 +0900

    20210120

--- Programmers/Hash/level2 (전화번호 목록)/level2.py
+++ Programmers/Hash/level2 (전화번호 목록)/level2.py
@@ -13,10 +13,22 @@ phone_book = ["123","456","789"]
 a = solution(phone_book)
 print(a)
 
-# p = "123"
-# q = "124312534123"
 
-# if p in q:
-#     print("yes")
-# else:
-#     print("no")
+# def solution(phoneBook):
+#     phoneBook = sorted(phoneBook)
+
+#     for p1, p2 in zip(phoneBook, phoneBook[1:]):
+#         if p2.startswith(p1):
+#             return False
+#     return True
+
+# def solution(phone_book): 
+#     for i in range(len(phone_book)): 
+#         pivot = phone_book[i] 
+#         for j in range(i+1, len(phone_book)):
+#             strlen = min(len(pivot), len(phone_book[j])) 
+#             if pivot[:strlen] == phone_book[j][:strlen]: 
+#                 return False 
+#     return True
+
+

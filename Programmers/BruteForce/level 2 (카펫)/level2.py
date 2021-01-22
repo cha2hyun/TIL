d8ed334431345f7705e7facd961b2fbb3ec7dde7
cha2hyun-dev <cha2hyun.dev@gmail.com>
2021-01-22 18:39:36 +0900

    20210122

---
+++ Programmers/BruteForce/level 2 (카펫)/level2.py
@@ -0,0 +1,8 @@
+def solution(brown, yellow):
+    x = (brown + 4 + ((brown + 4) ** 2 - 16 * (brown + yellow)) ** 0.5) / 4
+    y = (brown + yellow) // x
+    return [max(x,y), min(x,y)]
+    
+brown = 10
+yellow = 2
+   

4c30dcb19c945d8141f75974360000cda65edd24
cha2hyun-dev <cha2hyun.dev@gmail.com>
2021-02-03 10:46:22 +0900

    20210203

--- Programmers/Sort/level2 (H-index)/level2.py
+++ Programmers/Sort/level2 (H-index)/level2.py
@@ -1,13 +1,13 @@
 def solution(citations):
     citations.sort(reverse=True)
-    print(citations)
-    k = enumerate(citations, start=1)
-    print(list(k))
-    t = map(min, enumerate(citations, start=1))
-    print(list(t))
+    # print("citations = ", citations)
+    
+    # t = map(min, enumerate(citations, start=1))
+    # print("map(min, enumerate(citations, start=1))", list(t))
 
     answer = max(map(min, enumerate(citations, start=1)))
     return answer
 
 
 print(solution([3, 0, 6, 1, 5]))
+#sort로 정렬해서 가장 큰값부터 작은값으로 정렬한후, enumerate로 (index, value)형태로 묶는다. 그리고 최댓값(start = 1)부터 각 value에 대해 최솟값 value의 값을 min으로 추출하고, 이 추출된 값은 enumerate가 끝나는 citations 리스트의 크기에 해당하는 갯수가 나온다. 이들을 map으로 묶으면, 한 value의 입장에서 보는 최솟값 value의 집합이 나온다. 즉 h값들의 집합이나온다. h값중 최대값을 max로 뽑아서 출력하면 된다.

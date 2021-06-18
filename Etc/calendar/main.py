839d725bcee5e2d7df49ec05459f267d2d45f2df
cha2hyun-dev <cha2hyun.dev@gmail.com>
2021-06-18 18:18:02 +0900

    1,3,5토요일 구하기

---
+++ Etc/calendar/main.py
@@ -0,0 +1,19 @@
+#1,3,5째주 토요일 구하기
+from datetime import datetime, timedelta
+
+def get_date(y, m, d):    
+    s = f'{y:04d}-{m:02d}-{d:02d}'
+    return datetime.strptime(s, '%Y-%m-%d')
+
+def get_week_no(y, m, d):
+    target = get_date(y, m, d)
+    firstday = target.replace(day=1)
+    if firstday.weekday() == 6:
+        origin = firstday
+    elif firstday.weekday() < 6:
+        origin = firstday - timedelta(days=firstday.weekday() + 1)
+    else:
+        origin = firstday + timedelta(days=6-firstday.weekday())
+    return (target - origin).days // 7 + 1
+
+print(get_week_no(2027,5,2))

776da16e51b266f5718b710ba300b847d379134f
cha2hyun-dev <cha2hyun.dev@gmail.com>
2021-06-21 11:43:49 +0900

    crot

--- Etc/calendar/main.py
+++ Etc/calendar/main.py
@@ -5,15 +5,24 @@ def get_date(y, m, d):
     s = f'{y:04d}-{m:02d}-{d:02d}'
     return datetime.strptime(s, '%Y-%m-%d')
 
-def get_week_no(y, m, d):
-    target = get_date(y, m, d)
+def get_week_no():
+    target = datetime.today()
     firstday = target.replace(day=1)
+    
     if firstday.weekday() == 6:
         origin = firstday
     elif firstday.weekday() < 6:
         origin = firstday - timedelta(days=firstday.weekday() + 1)
     else:
         origin = firstday + timedelta(days=6-firstday.weekday())
+
+    print(target.weekday())
+
     return (target - origin).days // 7 + 1
 
-print(get_week_no(2027,5,2))
+print(get_week_no())
+
+today_1 = datetime.today().strftime('%Y-%m-%d')
+print(type(today_1))
+
+# print(datetime.today().strptime('%Y-%m-%d'))

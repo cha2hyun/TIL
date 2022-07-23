8aa1833f191cb70acdca81088c4e283db3d32469
cha2hyun <cha2hyun.dev@gmail.com>
2022-07-23 13:28:57 +0900

    20220723 프로그래머스  카카오블라인드 오픈채팅

--- Programmers/2019_KAKAO_BLIND/LV2_오픈채팅방/solution.py
+++ Programmers/2019_KAKAO_BLIND/LV2_오픈채팅방/solution.py
@@ -41,7 +41,7 @@ def solution2(record):
 
 # 아이디어 3
 # 거꾸로 돌지 않음
-def solution(record):
+def solution3(record):
     answer = []
     uids = {}    
     for rec in record:
@@ -58,4 +58,22 @@ def solution(record):
             
     return answer
 
-print(solution1(records))
+# 다른사람풀이 
+def solution(record):
+    answer = []
+    namespace = {}
+    printer = {'Enter':'님이 들어왔습니다.', 'Leave':'님이 나갔습니다.'}
+    for r in record:
+        rr = r.split(' ')
+        if rr[0] in ['Enter', 'Change']:
+            namespace[rr[1]] = rr[2]
+
+    for r in record:
+        if r.split(' ')[0] != 'Change':
+            answer.append(namespace[r.split(' ')[1]] + printer[r.split(' ')[0]])
+
+    return answer
+
+
+
+print(solution3(records))

778a7da0aa16ffff3f611ae690fb4c4dc3cececb
cha2hyun <cha2hyun.dev@gmail.com>
2022-10-20 11:16:39 +0900

    221020 43628

---
+++ Algorithm/programmers/lv3/42628/solution.py
@@ -0,0 +1,10 @@
+def solution(triangle):
+    triangle = [[0] + line + [0] for line in triangle]
+    print(triangle)
+    for i in range(1, len(triangle)):
+        for j in range(1, i+2):
+            triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])    
+    return max(triangle[-1])
+
+s = solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]])
+print(s)
